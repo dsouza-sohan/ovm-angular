6186f441a738dd4881aa54bb1b59ab8457bc4f19
SOhan <sohandsouza07@gmail.com>
2024-08-05 23:09:36 +0100

    completed login registration FE, needs work on CSS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ovn-angular' title`, () => {
+  it(`should have the 'ovm-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ovn-angular');
+    expect(app.title).toEqual('ovm-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ovn-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ovm-angular');
   });
 });
