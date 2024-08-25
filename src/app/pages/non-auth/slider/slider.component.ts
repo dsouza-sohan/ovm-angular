7a98774ec378fd9e799381c123600c5ac1996d7f
Marvin Delroy Aroza <developertom23@gmail.com>
2024-08-25 23:32:51 +0100

    feat: refactor dashboard

---
+++ src/app/pages/non-auth/slider/slider.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'ovm-slider',
+  standalone: true,
+  imports: [MatIcon],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss'
+})
+export class SliderComponent {
+
+}
