59a256fce1ee83745cae600b30549d129bc6d1ea
Marvin Delroy Aroza <developertom23@gmail.com>
2024-08-25 17:43:29 +0100

    feat: dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegistrationComponent } from './pages/auth/registration/registration.component';
+import { DashboardComponent } from './pages/non-auth/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: LoginComponent
+        component: DashboardComponent
     },
     {
         path: 'signup',
