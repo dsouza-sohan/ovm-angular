6905b2d350207944da5f2e0b3606e648d36d1f8d
Marvin Delroy Aroza <developertom23@gmail.com>
2024-09-19 23:27:01 +0100

    orders

--- src/app/core/services/orders.service.ts
+++ src/app/core/services/orders.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,21 @@ export class OrdersService {
         headers: headers,
       }
     );
+  }
+
+  //Login function
+  getOrderId(id: string) {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    headers = headers.append('Accept', 'application/json');
+    return this.http
+      .get<any>(`${environment.apiUrl}order/${id}`, {
+        headers: headers,
+      })
+      .pipe(
+        map((result: any) => {
+          return result;
+        })
+      );
   }
 }
--- src/app/pages/non-auth/orders/orders.component.ts
+++ src/app/pages/non-auth/orders/orders.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { OrdersService } from '../../../core/services/orders.service';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss',
 })
-export default class OrdersComponent {}
+export default class OrdersComponent {
+  orders: any;
+  private orderService = inject(OrdersService);
+
+  constructor() {
+    this.getOrderDetails();
+  }
+
+  getOrderDetails() {
+    this.orderService
+      .getOrderId(JSON.parse(localStorage.getItem('currentUser') || '{}').id)
+      .subscribe((res: { data: any }) => {
+        this.orders = res.data;
+      });
+  }
+}
